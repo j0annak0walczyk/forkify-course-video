d2476a4a534a7334eb6913529832dbb4f8a51f31
j0annak0walczyk <j.kowalczyk0070@gmail.com>
2023-10-13 11:39:35 +0200

    Add new welcome feature

--- controller.js
+++ controller.js
@@ -118,6 +118,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -126,6 +130,6 @@ const init = function () {
   searchView.addHendlerSearch(controlSearchResult);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('ASIA!!!');
+  newFeature();
 };
 init();
